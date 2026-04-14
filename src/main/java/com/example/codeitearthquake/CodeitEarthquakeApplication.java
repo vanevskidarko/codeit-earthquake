7c8ae17606d099490b7c17b5ac35da5217a39a1d
Darko Vanevski <darko.vanevski@students.finki.ukim.mk>
2026-04-14 19:16:45 +0200

    initial setup

---
+++ src/main/java/com/example/codeitearthquake/CodeitEarthquakeApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeitearthquake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeitEarthquakeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeitEarthquakeApplication.class, args);
+    }
+
+}
